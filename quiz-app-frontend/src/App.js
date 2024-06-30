76e5909b197b06782f074a93f3dfba6a3287ea00
= <=>
2024-06-30 13:23:34 +0530

    routing

--- quiz-app-frontend/src/App.js
+++ quiz-app-frontend/src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {BrowserRouter, Route,Routes} from 'react-router-dom';
+import { Header } from './components/Header';
+import { Login } from './components/Login';
+import { Register } from './components/Register';
+import { Test } from './components/Test';
+import { Resources } from './components/Resources';
 function App() {
   return (
-    <div className="App">
-      app updated
-    </div>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path = "/login" element={<Login/>}></Route>
+        <Route path = "/register" element = {<Register/>}></Route>
+        <Route path = "/resources" element = {<Resources/>}></Route>
+        <Route path = "/test" element = {<Test/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
