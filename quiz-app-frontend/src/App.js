057db44ef3c66a1f102592d721aeda06f34bda15
SakethGunda9603 <gundasaketh9603@gmail.com>
2024-06-30 11:55:23 +0530

    com

--- quiz-app-frontend/src/App.js
+++ quiz-app-frontend/src/App.js
@@ -4,11 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       app 
-=======
       app updated
->>>>>>> main
     </div>
   );
 }
--- quiz-app-frontend/src/components/Header.js
+++ quiz-app-frontend/src/components/Header.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Header = () => {
   return (
-    <div>Header updated</div>
+    <div>Header updated123</div>
   )
 }
