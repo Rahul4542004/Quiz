b2a08d825a848704f04ffdc9f77e16c315a36dee
= <=>
2024-07-17 13:25:56 +0530

    mysql server deployed

---
+++ quiz-app-backend/src/main/java/com/example/demo/entity/Answers.java
@@ -0,0 +1,23 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "answers")
+public class Answers {
+    @Id
+    private Long id;
+    @Column(unique = true)
+    private String options;
+}
---
+++ quiz-app-backend/src/main/java/com/example/demo/entity/OSQuestions.java
@@ -0,0 +1,43 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.Set;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "os_questions")
+public class OSQuestions {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "option_a")
+    private String optionA;
+
+    @Column(name = "option_b")
+    private String optionB;
+
+    @Column(name = "option_c")
+    private String optionC;
+
+    @Column(name = "option_d")
+    private String optionD;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "os_question_answers",
+            joinColumns = @JoinColumn(name = "q_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "ans_id", referencedColumnName = "id")
+    )
+    private Set<Answers> answers;
+}
