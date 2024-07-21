b9c3b1e52320cacfff4c25af5dbc60a0bee3af05
SakethGunda9603 <gundasaketh9603@gmail.com>
2024-07-21 21:13:55 +0530

    main branch created

--- quiz-app-frontend/src/components/Header.js
+++ quiz-app-frontend/src/components/Header.js
@@ -1,7 +1,114 @@
-import React from 'react'
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
 
-export const Header = () => {
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+function Header({ isLoggedIn }) {
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <div>Header </div>
-  )
+    <AppBar
+      position="static"
+      sx={{
+        background:
+          "linear-gradient(110.1deg, rgb(34, 126, 34) 2.9%, rgb(168, 251, 60) 90.3%)",
+      }}
+    >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#"
+            sx={{
+              mr: 2,
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            QUIZZ-APP
+          </Typography>
+
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            {isLoggedIn ? (
+              <>
+                <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar
+                      alt="User Avatar"
+                      src="/static/images/avatar/2.jpg"
+                    />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  sx={{ mt: "45px" }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  {settings.map((setting) => (
+                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                      <Typography textAlign="center">{setting}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </>
+            ) : (
+              <Button
+                color="inherit"
+                sx={{
+                  background:
+                    "linear-gradient(98.3deg, rgb(255, 220, 0) 10.6%, rgb(255, 180, 0) 97.7%)",
+                  color: "#000",
+                  borderRadius: "4px",
+                  "&:hover": {
+                    background:
+                      "linear-gradient(98.3deg, rgb(0, 0, 0) 10.6%, rgb(255, 0, 0) 97.7%)",
+                  },
+                  transition: "background 0.3s ease",
+                }}
+              >
+                LOGIN/REGISTER
+              </Button>
+            )}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
+
+export default Header;
--- quiz-app-frontend/src/components/Main.js
+++ quiz-app-frontend/src/components/Main.js
@@ -1,7 +1,63 @@
-import React from 'react'
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
+import { Box } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
-export const Main = () => {
+// Define styles for the cards
+const StyledCard = styled(Card)({
+  maxWidth: 400,
+  display: "flex",
+  flexDirection: "column",
+});
+
+export default function Main() {
+  const navigate = useNavigate();
+
   return (
-    <div>Main</div>
-  )
+    <Stack
+      direction="row"
+      spacing={50} // Space between cards
+      padding={20} // Padding around the Stack container
+    >
+      <StyledCard onClick={() => navigate("/resources")}>
+        <CardMedia
+          sx={{ height: 240 }}
+          image="/quiz1.jpg"
+          title="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            RESOURCES
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica.
+          </Typography>
+        </CardContent>
+      </StyledCard>
+
+      <StyledCard onClick={() => navigate("/test")}>
+        <CardMedia
+          sx={{ height: 240 }}
+          image="/quiz2.jpg"
+          title="green iguana"
+        />
+
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            TEST
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica.
+          </Typography>
+        </CardContent>
+      </StyledCard>
+    </Stack>
+  );
 }
